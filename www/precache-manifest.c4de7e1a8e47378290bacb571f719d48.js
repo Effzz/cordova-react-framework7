93279aebe51c33b5dce8b972cf3bd2a69172e90e
Effzz <efendy.s.89@gmail.com>
2019-04-11 23:07:41 +0700

    init

---
+++ www/precache-manifest.c4de7e1a8e47378290bacb571f719d48.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e79bfd88537def476913f3ed52f4f4b3",
+    "url": "./static/media/MaterialIcons-Regular.e79bfd88.eot"
+  },
+  {
+    "revision": "b0ccc8c6146bcb8d2e0a",
+    "url": "./static/css/main.5c19f7f0.chunk.css"
+  },
+  {
+    "revision": "61bbf26d99172b62fb8eb5b03585db72",
+    "url": "./static/media/Framework7Icons-Regular.61bbf26d.ttf"
+  },
+  {
+    "revision": "c0b71228d53cbe1667eb",
+    "url": "./static/js/1.c0b71228.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "570eb83859dc23dd0eec423a49e147fe",
+    "url": "./static/media/MaterialIcons-Regular.570eb838.woff2"
+  },
+  {
+    "revision": "012cf6a10129e2275d79d6adac7f3b02",
+    "url": "./static/media/MaterialIcons-Regular.012cf6a1.woff"
+  },
+  {
+    "revision": "b0ccc8c6146bcb8d2e0a",
+    "url": "./static/js/main.b0ccc8c6.chunk.js"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "./static/media/MaterialIcons-Regular.a37b0c01.ttf"
+  },
+  {
+    "revision": "d5e7e689e790dd5f60eaa438da65581f",
+    "url": "./static/media/Framework7Icons-Regular.d5e7e689.eot"
+  },
+  {
+    "revision": "0a51e7bba83f6ce6ef65b35a9ba4e0f2",
+    "url": "./static/media/Framework7Icons-Regular.0a51e7bb.woff2"
+  },
+  {
+    "revision": "0cefb5cc6ff6df9509ed193cff79de7c",
+    "url": "./static/media/Framework7Icons-Regular.0cefb5cc.woff"
+  },
+  {
+    "revision": "c0b71228d53cbe1667eb",
+    "url": "./static/css/1.2c48d482.chunk.css"
+  },
+  {
+    "revision": "ac132cabb82a58cb8ccb8a52dd9d6024",
+    "url": "./index.html"
+  }
+];
